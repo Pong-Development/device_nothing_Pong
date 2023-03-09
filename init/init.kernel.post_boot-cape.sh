d4810e1191ff0263dc6819e80624746561cca424
Jake Weinstein <jake@aospa.co>
2023-03-10 07:58:57 +0900

    Pong: init: Do not allow foreground tasks to run on CPU7
    Gold+ cores use significantly more power than Gold cores.
    Only allow top-app tasks to run on Gold+ to save power
    and prioritize the most critical threads.
    
    Change-Id: I95e5e18dfe0e5b6818eee1c589a22959bea4868a
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- init/init.kernel.post_boot-cape.sh
+++ init/init.kernel.post_boot-cape.sh
@@ -57,6 +57,7 @@ echo 325 > /proc/sys/walt/walt_low_latency_task_threshold
 # cpuset parameters
 echo 0-1 > /dev/cpuset/background/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
+echo 0-6 > /dev/cpuset/foreground/cpus
 
 # Set restricted cpuset to the same CPUs as system-background
 cat /dev/cpuset/system-background/cpus > /dev/cpuset/restricted/cpus
