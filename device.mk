a1a49fc1df7dd51d55b276e4ba7e37a86bd89391
Aaron Kling <webgeek1234@gmail.com>
2023-08-27 12:54:32 -0500

    Pong: Move most agm and pal targets to source built
    [chandu078]: Build libagmmixer.so from source
    
    Change-Id: I6ca0c5396a3b583b37e029f5c98ff13127db65a6
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -50,12 +50,19 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl \
+    agmcap \
+    agmcompressplay \
+    agmplay \
     audio.bluetooth.default \
     audio.primary.taro \
     audio.r_submix.default \
     audio.usb.default \
     audioadsprpcd \
     libaudiopreprocessing \
+    libagm_compress_plugin \
+    libagm_mixer_plugin \
+    libagmmixer \
+    libagm_pcm_plugin \
     libbatterylistener \
     libbundlewrapper \
     libdownmix \
@@ -321,7 +328,8 @@ PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libcodec2_hidl@1.0.vendor \
-    libcodec2_vndk.vendor
+    libcodec2_vndk.vendor \
+    libpalclient
 
 # msm_irqbalance
 PRODUCT_COPY_FILES += \
