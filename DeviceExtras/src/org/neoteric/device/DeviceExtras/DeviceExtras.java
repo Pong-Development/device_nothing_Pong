b9373f32ed59d27e667e880445b29a54a7775841
AnierinBliss <anierin@evolution-x.org>
2023-12-26 22:12:10 +0530

    Pong: Introduce DeviceExtras
    Features:
    - PowerShare switch and Tile
    - More to come soon :)
    
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: AliHasan7671 <AHB7671@gmail.com>
    Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-bt: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Co-authored-by: Adrian DC <radian.dc@gmail.com>
    Co-authored-by: SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
    Co-authored-by: Michele Bono <bono.michele94@gmail.com>
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
    Co-authored-by: Ziauddin Sameer <ziauddinsameer@gmail.com>
    Signed-off-by: HELLBOY017 <abhaygill017@gmail.com>

---
+++ DeviceExtras/src/org/neoteric/device/DeviceExtras/DeviceExtras.java
@@ -0,0 +1,83 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2021 The dot X Project
+* Copyright (C) 2018-2021 crDroid Android Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.neoteric.device.DeviceExtras;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreference;
+import androidx.preference.TwoStatePreference;
+
+import org.neoteric.device.DeviceExtras.FileUtils;
+import org.neoteric.device.DeviceExtras.R;
+
+public class DeviceExtras extends PreferenceFragment {
+    public static final String KEY_POWERSHARE_SWITCH = "powershare";
+
+    private static TwoStatePreference mPowerShareModeSwitch;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this.getContext());
+        addPreferencesFromResource(R.xml.main);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        // PowerShare
+        mPowerShareModeSwitch = (TwoStatePreference) findPreference(KEY_POWERSHARE_SWITCH);
+        if (PowerShareModeSwitch.isSupported()) {
+            mPowerShareModeSwitch.setChecked(PowerShareModeSwitch.isCurrentlyEnabled(this.getContext()));
+            mPowerShareModeSwitch.setOnPreferenceChangeListener(new PowerShareModeSwitch());
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this.getContext());
+    }
+
+    private void removePref(Preference pref) {
+        PreferenceGroup parent = pref.getParent();
+        if (parent == null) {
+            return;
+        }
+        parent.removePreference(pref);
+        if (parent.getPreferenceCount() == 0) {
+            removePref(parent);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        // Respond to the action bar's Up/Home button
+        case android.R.id.home:
+            getActivity().finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ DeviceExtras/src/org/neoteric/device/DeviceExtras/Startup.java
@@ -0,0 +1,54 @@
+/*
+* Copyright (C) 2013 The OmniROM Project
+* Copyright (C) 2021 The dot OS Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.neoteric.device.DeviceExtras;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
+
+public class Startup extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, final Intent bootintent) { 
+        boolean enabled = false;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_POWERSHARE_SWITCH, false);
+        if (enabled) {
+        restore(PowerShareModeSwitch.getFile(), enabled);
+        }
+    }
+
+    private void restore(String file, boolean enabled) {
+        if (file == null) {
+            return;
+        }
+        if (enabled) {
+            FileUtils.writeValue(file, "1");
+        }
+    }
+
+    private void restore(String file, String value) {
+        if (file == null) {
+            return;
+        }
+        FileUtils.writeValue(file, value);
+    }
+}
