2326184ff901f9a68a6ca30bb267eafd1789a0d3
someone5678 <someone5678@users.noreply.github.com>
2024-06-08 22:59:05 +0900

    Pong: Use QCOM USB gadget HAL
    * Newer SoC's use gadget HAL
    
    Change-Id: Ib3383bc1173db22c539b2073d72419c42fcf92cc
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- device.mk
+++ device.mk
@@ -291,8 +291,6 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.recovery.rc \
     init.qcom.sh \
-    init.qcom.usb.rc \
-    init.qcom.usb.sh \
     init.target.rc \
     ueventd.nt.rc \
     ueventd.qcom.rc
@@ -526,7 +524,13 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.3-service-qti \
+    android.hardware.usb.gadget@1.2-service-qti
+
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    usb_compositions.conf
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
