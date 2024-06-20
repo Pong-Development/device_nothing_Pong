6096ef9e11c0f934fdf37f437d09b86446c782fc
Ghosuto <clash.raja10@gmail.com>
2024-06-20 15:44:52 +0530

    Pong: Move init.kernel.post_boot.sh in dt

--- device.mk
+++ device.mk
@@ -262,6 +262,7 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.nt.rc \
     init.qcom.early_boot.sh \
+    init.kernel.post_boot.sh \
     init.qcom.rc \
     init.qcom.recovery.rc \
     init.qcom.sh \
