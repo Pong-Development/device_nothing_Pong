eaeccea2ae0591d0c2b04326eee940eae26184bf
PRANAY BHARGAV BHAVARAJU <quic_pbhavara@quicinc.com>
2024-06-24 11:28:49 +0530

    Pong: Added the UID for configuring SNXXX SB services
    Updated the file with the Strongbox, weaver
    and authsecret service entries for SNXXX eSE.
    
    Change-Id: I0df1817bf49f68993b4789506641778d4aeaf285
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- config.fs
+++ config.fs
@@ -49,6 +49,24 @@ value:2916
 [AID_VENDOR_THALES_AUTHSECRET]
 value:2917
 
+[vendor/bin/hw/android.hardware.security.keymint-service.strongbox-nxp]
+mode: 0755
+user: AID_VENDOR_NXP_STRONGBOX
+group: AID_SYSTEM
+caps: SYS_ADMIN SYS_NICE WAKE_ALARM
+
+[vendor/bin/hw/android.hardware.weaver-service.nxp]
+mode: 0755
+user: AID_VENDOR_NXP_WEAVER
+group: AID_SYSTEM
+caps: SYS_ADMIN SYS_NICE WAKE_ALARM
+
+[vendor/bin/hw/android.hardware.authsecret-service.nxp-qti]
+mode: 0755
+user: AID_VENDOR_NXP_AUTHSECRET
+group: AID_SYSTEM
+caps: SYS_ADMIN SYS_NICE WAKE_ALARM
+
 [vendor/bin/hw/android.hardware.security.keymint-service.strongbox-thales]
 mode: 0755
 user: AID_VENDOR_THALES_STRONGBOX
