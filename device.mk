4963179d4d70bfea29857064bbfd98c0bc344153
Aditya R <gh0strider.2k18.reborn@gmail.com>
2024-08-06 02:17:11 +0530

    phone2: Checkout Dolby from hardware_xiaomi
    Based on existing dirac implementation and observing stock
    sound effects app and daxservice.
    
    history -
    - Add launcher icon
    - Introduce graphical equalizer
    - Do not set volume leveler amount
    - Restore all settings upon bootup
    - Rewrite in Kotlin
    - Restore dolby profile on audio changes
    - Guard debug logging
    - Implement profile-specific Dolby settings
    - Remove play/pause hack while toggling Dolby
    - Set proper summary for dolby settings
    - Always refresh playback if status changed
    - Introduce Dolby Atmos
    
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- device.mk
+++ device.mk
@@ -105,6 +105,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 
 # Dolby
+PRODUCT_PACKAGES += \
+    PartsDolby
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/audio/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
