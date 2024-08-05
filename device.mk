aa47fd6c2e293bead58a46719c8ecb51693d5fcf
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-06 00:29:20 +0530

    phone2: Link some libs against v33 libstagefright_foundation
    * fixes crash and/or high CPU usage
    
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Co-authored-by: Woomymy <woomy@woomy.be>
    Change-Id: Ib72b775dae677f3085d0b0e2afcbb709d8690063

--- device.mk
+++ device.mk
@@ -104,6 +104,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.config-V5-ndk_platform.vendor \
     vendor.qti.hardware.memtrack-service
 
+# Dolby
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
