e5fedb39f3c584777210c163f2ff4463457df01b
Aaron Kling <webgeek1234@gmail.com>
2024-08-18 17:05:38 -0500

    phone2: Use v1 rkp interface for keymint
    Change-Id: I281efe89458fefc37a25aa7c2555b9bf92bbdc14

--- extract-files.sh
+++ extract-files.sh
@@ -58,7 +58,7 @@ function blob_fixup() {
     case "${1}" in
         vendor/bin/hw/android.hardware.security.keymint-service-qti|vendor/lib64/libqtikeymint.so)
             [ "$2" = "" ] && return 0
-            grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || ${PATCHELF} --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
+            grep -q "android.hardware.security.rkp-V1-ndk.so" "${2}" || ${PATCHELF} --add-needed "android.hardware.security.rkp-V1-ndk.so" "${2}"
             ;;
         vendor/bin/hw/dolbycodec2 | vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service | vendor/bin/hw/vendor.dolby.media.c2@1.0-service)
             "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
