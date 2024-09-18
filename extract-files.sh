ee3db76c40cd3f2d3aeef145f36359bc847fdf2e
Ghosuto <clash.raja10@gmail.com>
2024-09-18 22:17:35 +0530

    Pong: Drop useless lib patch
    - no need to patch audio.primary.taro.so, dolby works fine without this
    
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -64,9 +64,6 @@ function blob_fixup() {
         vendor/bin/hw/dolbycodec2 | vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service | vendor/bin/hw/vendor.dolby.media.c2@1.0-service)
             "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
-        vendor/lib64/hw/audio.primary.taro.so)
-            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
-            ;;
         vendor/etc/media_codecs_cape.xml|vendor/etc/media_codecs_cape_vendor.xml)
             sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio)/d" "${2}"
             ;;
