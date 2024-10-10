c7db00e4b3a01f2d88c766fefeea3529b0df2846
SamarV-121 <samar@samarv121.dev>
2024-10-10 23:02:05 +0530

    Pong: Link older libcrypto for widevine blobs
    google/boringssl@e202e51
    
    Change-Id: Ic32d5103d42cd28ccbc8055e766288dfefc1c02d

--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
             ;;
+        vendor/lib64/mediadrm/libwvdrmengine.so|vendor/lib64/libwvhidl.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcrypto-v33.so" "${2}" || "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "$2"
+            ;;
     esac
 }
 
