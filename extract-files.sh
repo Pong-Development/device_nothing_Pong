a93ef3e1eeb38698891fe40c5da125e3dbbcc28d
Arian <arian.kulmer@web.de>
2024-10-10 22:59:49 +0530

    Pong: Allow gettid syscall for atfwd@2.0
    09-18 17:27:17.797     1     1 I init    : ... started service 'vendor.atfwd' has pid 6264
    09-18 17:27:17.805  6264  6264 W ATFWD-daemon: libminijail[6264]: failed to get path of fd 3: No such file or directory
    09-18 17:27:17.805  6264  6264 W ATFWD-daemon: libminijail[6264]: allowing syscall: connect
    09-18 17:27:17.805  6264  6264 W ATFWD-daemon: libminijail[6264]: allowing syscall: fcntl
    09-18 17:27:17.805  6264  6264 W ATFWD-daemon: libminijail[6264]: allowing syscall: sendto
    09-18 17:27:17.805  6264  6264 W ATFWD-daemon: libminijail[6264]: allowing syscall: socket
    09-18 17:27:17.805  6264  6264 W ATFWD-daemon: libminijail[6264]: allowing syscall: writev
    09-18 17:27:17.806  6264  6264 W ATFWD-daemon: libminijail[6264]: logging seccomp filter failures
    09-18 17:27:17.808  6264  6264 E ATFWD-daemon: libminijail[6264]: blocked syscall: gettid
    09-18 17:27:17.826     1     1 I init    : Service 'vendor.atfwd' (pid 6264) received signal 31
    09-18 17:27:17.826     1     1 I init    : Sending signal 9 to service 'vendor.atfwd' (pid 6264) process group...
    
    Change-Id: I9a556785b9bbda0f08974ad2b367c77c649ee01b

--- extract-files.sh
+++ extract-files.sh
@@ -67,6 +67,10 @@ function blob_fixup() {
         vendor/etc/media_codecs_cape.xml|vendor/etc/media_codecs_cape_vendor.xml)
             sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio)/d" "${2}"
             ;;
+        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+            [ "$2" = "" ] && return 0
+            grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
+            ;;
     esac
 }
 
