ca3b35f2343185a74790347276ed067982208347
Ghosuto <clash.raja10@gmail.com>
2024-10-14 05:52:04 +0000

    Pong: Switch to meteoric kernel
    - https://github.com/HELLBOY017/kernel_nothing_sm8475
    
    This reverts commit 4200570ab2dd5b19913f55330a854ad6da80de13.
    
    This reverts commit fc2178bf0469b6b82ad1effdd3b80008758eac26.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,11 +92,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image
 
 TARGET_KERNEL_SOURCE := kernel/nothing/sm8475
-TARGET_KERNEL_CONFIG := \
-    gki_defconfig \
-    vendor/waipio_GKI.config \
-    vendor/nothing/waipio_GKI.config \
-    vendor/debugfs.config
+TARGET_KERNEL_CONFIG := vendor/meteoric_defconfig ksu.config
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist
