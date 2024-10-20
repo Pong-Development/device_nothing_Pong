6452054987c91fecb648832ac6c21616950a351b
Ghosuto <clash.raja10@gmail.com>
2024-10-20 23:50:36 +0530

    phone2: init: Set swappiness to 60%
    - 100 bit aggressive/overkill
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- init/bin/init.kernel.post_boot.sh
+++ init/bin/init.kernel.post_boot.sh
@@ -129,7 +129,7 @@ function configure_memory_parameters() {
 
 	configure_zram_parameters
 	configure_read_ahead_kb_values
-	echo 100 > /proc/sys/vm/swappiness
+	echo 60 > /proc/sys/vm/swappiness
 
 	# Disable periodic kcompactd wakeups. We do not use THP, so having many
 	# huge pages is not as necessary.
