94638828b025837a3127ccbd9085e20e20253db8
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-01-31 03:52:23 +0000

    phone2: dolby: Reduce padding and matches bg color to default
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- dolby/src/co/aospa/dolby/xiaomi/geq/ui/EqualizerScreen.kt
+++ dolby/src/co/aospa/dolby/xiaomi/geq/ui/EqualizerScreen.kt
@@ -7,6 +7,7 @@
 package co.aospa.dolby.xiaomi.geq.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.android.settingslib.spa.framework.theme.settingsBackground
@@ -24,12 +26,11 @@ fun EqualizerScreen(
     viewModel: EqualizerViewModel,
     modifier: Modifier = Modifier
 ) {
-    Surface(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(SettingsDimension.itemPadding)
             .then(modifier),
-        color = MaterialTheme.colorScheme.settingsBackground
     ) {
         Column(
             verticalArrangement = Arrangement.Top,
