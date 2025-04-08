d0cd0ab8a68a012ac57f54af8bea2fd67350e0a7
kenway214 <kenway214@outlook.com>
2025-04-08 16:33:19 +0000

    Pong: Introduce GameBar v2.0
    - adapt for Pong
    
    Co-authored-by: Ghosuto <clash.raja10@gmail.com>

--- device.mk
+++ device.mk
@@ -187,6 +187,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Gamebar
+PRODUCT_PACKAGES += \
+    GameBar
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
