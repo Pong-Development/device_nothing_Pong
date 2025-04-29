91c16cfc73ec3622b196c7346bf2ad5fc5bc6d04
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    Pong: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- device.mk
+++ device.mk
@@ -243,6 +243,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/charging_en)
+
 # Logging
  SPAMMY_LOG_TAGS := \
     Diag_Lib \
