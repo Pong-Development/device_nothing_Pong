cb10b69416a0af5acd47ac7a34b4ac817ab97ba1
Govind R Sekhar <quic_gsekhar@quicinc.com>
2025-04-30 19:31:42 +0000

    Pong: Add missing layer buffer slots cache clear property
    Add missing layer buffer slots cache clear property used to enable
    IComposer3Client setLayerCommands API to explicitly clear buffer
    cache slots for a layer.
    
    [adithya2306]:
    Enable it as it's supported by 5.10+ display HAL and is disabled by
    default.

--- device.mk
+++ device.mk
@@ -187,6 +187,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.clear_slots_with_set_layer_buffer=true
+
 # Gamebar
 PRODUCT_PACKAGES += \
     GameBar
