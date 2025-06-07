59bb45079a6b0d516dff37a1f537a9761ed88bf6
LuK1337 <priv.luk@gmail.com>
2025-06-07 11:38:14 +0530

    Pong: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643
    Signed-off-by: Chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -323,6 +323,10 @@ PRODUCT_PACKAGES += \
     NTSystemUIResTarget \
     NTWifiResTarget
 
+# NCM overlays
+PRODUCT_PACKAGES += \
+    NcmTetheringOverlay
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
