2f53e3514086b18e8572cf315339146fac1a0ce6
Bruno Martins <bgcngm@gmail.com>
2025-06-07 12:15:59 +0530

    Pong: Move and recategorize compat matrix and manifest flags
    Qualcomm also sets these in (common) product makefiles.
    
    Change-Id: I7139cba92454106fbca5f6105b966a43e70fe9d6
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,19 +67,6 @@ TARGET_SCREEN_DENSITY := 420
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
-# HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(DEVICE_PATH)/device_framework_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
-
-DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
-DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += \
-    $(DEVICE_PATH)/manifest.xml \
-    hardware/qcom-caf/sm8450/audio/primary-hal/configs/common/manifest_non_qmaa.xml \
-    hardware/qcom-caf/sm8450/audio/primary-hal/configs/common/manifest_non_qmaa_extn.xml
-
 # Kernel
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
--- device.mk
+++ device.mk
@@ -481,6 +481,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# VINTF
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(LOCAL_PATH)/device_framework_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := device/nothing/Pong/framework_manifest.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := \
+    $(AUDIO_HAL_DIR)/configs/common/manifest_non_qmaa.xml \
+    $(AUDIO_HAL_DIR)/configs/common/manifest_non_qmaa_extn.xml \
+    $(LOCAL_PATH)/manifest.xml
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
