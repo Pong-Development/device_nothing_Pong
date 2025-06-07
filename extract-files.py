d2db75c266eb4fa51654b4c06c283cce8dde47de
LuK1337 <priv.luk@gmail.com>
2025-06-07 11:17:50 +0530

    Pong: libwfdservice_shim_v1 -> libwfdservice_shim
    Change-Id: Idf65dd61a2de80dfcbf72a8b75b6ca59234df573
    Signed-off-by: Chandu078 <chandudyavanapelli03@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -50,7 +50,7 @@ lib_fixups: lib_fixups_user_type = {
 
 blob_fixups: blob_fixups_user_type = {
     'system_ext/bin/wfdservice64': blob_fixup()
-        .add_needed('libwfdservice_shim_v1.so'),
+        .add_needed('libwfdservice_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so')
