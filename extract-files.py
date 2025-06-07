374036a3b4c606c965215f3a3ccec96bedb8aa03
Aaron Kling <webgeek1234@gmail.com>
2025-06-07 17:11:46 +0530

    Pong: Drop qvr/qxr
    Does not seem that useful, and conflicts with the
    libgrpc++_unsecure package if elf checks are turned
    on in extract files.
    The libgrpc++_unsecure package builds into the
    libgrpc++_unsecure-1.61.1.so binary, but the QVR blob
    needs libgrpc++_unsecure.so.
    Rather than mess with extract files to fix it
    some more, just get rid of it.
    
    Change-Id: I53556a5b3452138341de6e1c1556aabb7cd8d711
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -83,8 +83,6 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libutils.so'),
     'vendor/lib64/libqcodec2_core.so': blob_fixup()
         .add_needed('libcodec2_shim.so'),
-    'vendor/lib64/vendor.qti.hardware.qxr-V1-ndk_platform.so': blob_fixup()
-        .replace_needed('android.hardware.common-V2-ndk_platform.so', 'android.hardware.common-V2-ndk.so'),
     ('vendor/lib64/libqcrilNr.so', 'vendor/lib64/libril-db.so'): blob_fixup()
         .binary_regex_replace(rb'persist\.vendor\.radio\.poweron_opt', b'persist.vendor.radio.poweron_ign'),
 }  # fmt: skip
