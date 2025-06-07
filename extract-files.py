62ad8fd02859a3452f2f882e31c5b5d772c1fc75
Cosmin Tanislav <demonsingur@gmail.com>
2025-06-07 15:47:33 +0530

    Pong: Remove unused media codecs and profiles
    On CAF, the ro.media.xml_variant.codecs is used for both codecs and
    profiles.
    
    media_profiles_vendor.xml does not exist on stock, even if
    media.settings.xml is set to /vendor/etc/media_profiles_vendor.xml.
    
    Setting media.settings.xml on CAF shortcircuits the loading to
    check /vendor/etc/media_profiles + ro.media.xml_variant.codecs + .xml.
    
    Setting ro.media.xml_variant.codecs on CAF makes it load
    /vendor/etc/media_codecs + ro.media.xml_variant.codecs + _vendor.xml.
    
    ro.media.xml_variant.codecs is set by init.qti.media.sh.
    
    Setting ro.media.xml_variant.profiles on AOSP makes it load
    /vendor/etc/media_codecs + ro.media.xml_variant.profiles + .xml.
    
    Remove all unused media codecs, and switch to stock media codecs
    while using AOSP logic by renaming media_codecs_cape_vendor.xml to
    media_codecs_cape.xml.
    
    Change-Id: Ibbea90ecb63b0cb611d50fbb6e7cbfa5f2d4d9d6
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -73,7 +73,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
         .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
         .add_needed('android.hardware.security.rkp-V1-ndk.so'),
-    ('vendor/etc/media_codecs_cape.xml', 'vendor/etc/media_codecs_cape_vendor.xml'): blob_fixup()
+    'vendor/etc/media_codecs_cape.xml': blob_fixup()
         .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', ''),
     ('vendor/lib64/libarcsoft_dark_vision_raw.so', 'vendor/lib64/libarcsoft_high_dynamic_range_v5.so'): blob_fixup()
         .clear_symbol_version('remote_handle_close')
