3c7188bcfd7919cc06f3acfc481aa2637b2756ee
Omkar Chandorkar <gotenksIN@aospa.co>
2025-06-09 15:12:24 +0000

    Pong: overlay: Add overlay for Nfc package
    Change-Id: I7947aa7e2aed624c1f9f19836d9a52ed974fa251
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -329,6 +329,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NTCarrierConfigResTarget \
     NTFrameworksResTarget \
+    NTNfcResTarget \
     NTSettingsProviderResTarget \
     NTSettingsResTarget \
     NTSystemUIResTarget \
