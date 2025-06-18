2e7bde91ff6ad377a28fae68ccecff8bba0af1b0
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-18 07:21:29 +0000

    Pong: Optimize dalvik heap config for performance
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- device.mk
+++ device.mk
@@ -120,7 +120,13 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.capabilityconfigstore@1.0.vendor
 
 # Dalvik
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+PRODUCT_VENDOR_PROPERTIES += \
+    dalvik.vm.heapstartsize?=16m \
+    dalvik.vm.heapgrowthlimit?=384m \
+    dalvik.vm.heapsize?=512m \
+    dalvik.vm.heaptargetutilization?=0.75 \
+    dalvik.vm.heapminfree?=512k \
+    dalvik.vm.heapmaxfree?=8m
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
