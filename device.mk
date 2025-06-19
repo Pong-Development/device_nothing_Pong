8edd21d5dcfd8b6505bb2f769b43329652407d4d
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-06-19 05:13:25 +0000

    Pong: Set libcameraservice ext lib via soong config
    Change-Id: I2bb052c75d9a7765a554372be1eceba25e324402

--- device.mk
+++ device.mk
@@ -116,6 +116,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
 $(call soong_config_set,camera,package_name,com.nothing.camera)
+$(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.Pong)
 
 # Charging
 PRODUCT_PACKAGES += \
