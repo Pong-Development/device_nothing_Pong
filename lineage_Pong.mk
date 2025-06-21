d48fa34e63c6c7f7c1dcc794774288f525555140
Ghosuto <clash.raja10@gmail.com>
2025-06-21 16:09:05 +0000

    Pong: Axion bringup

--- lineage_Pong.mk
+++ lineage_Pong.mk
@@ -20,6 +20,34 @@ PRODUCT_MANUFACTURER := Nothing
 PRODUCT_BRAND := Nothing
 PRODUCT_MODEL := A065
 
+# Singing keys
+$(call inherit-product, vendor/mine/keys.mk)
+
+# Define rear camera specs
+AXION_CAMERA_REAR_INFO := 50,50
+
+# Blur
+TARGET_ENABLE_BLUR := true
+
+# Define front camera specs
+AXION_CAMERA_FRONT_INFO := 32
+
+# Maintainer name
+AXION_MAINTAINER := GHOST_|_ゴースト
+
+# Processor name
+AXION_PROCESSOR := Snapdragon®_8+_Gen_1 
+
+# Default core groups (if not overridden by the builder)
+AXION_CPU_SMALL_CORES := 0,1,2,3
+AXION_CPU_BIG_CORES := 4,5,6,7
+AXION_CPU_UNLIMIT_UI := 0-7
+AXION_CPU_BG := 0-2
+AXION_CPU_FG := 0-7
+AXION_CPU_LIMIT_BG := 0-1
+AXION_CPU_LIMIT_UI := 0-4
+AXION_DEBUGGING_ENABLED := false
+
 PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-nothing
