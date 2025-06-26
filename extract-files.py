d96c4688516ea468d0c580703d9cc9bc8d7c3329
hiroshi <hiroshi@tanuki.page>
2025-06-26 02:06:28 +0100

    Pong: add libaudioclient_shim for libwfdservice
    Co-authored-by: ralph950412 <ralph950412@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -59,7 +59,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libinput_shim.so')
         .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
-        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so')
+        .add_needed('libaudioclient_shim.so'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libofflineproc_jni.so': blob_fixup()
