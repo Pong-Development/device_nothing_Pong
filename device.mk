d844fc2a263b07cc1ee0ad14276e51954020d63c
Matt Buckley <mattbuckley@google.com>
2025-07-02 17:17:31 +0000

    Pong: Disable HWUI adpf by default and enable in device.mk file
    * Make HWUI disabled by default for vendors
     * Explicitly enable in supported devices
     * Consolidate flags for HWUI and SF in hardware code
    
    Bug: b/261628396
    Test: manual
    Change-Id: I7352f933993770498d3403aeb4e45fd3f1afab88

--- device.mk
+++ device.mk
@@ -349,6 +349,11 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
+# Enable adpf cpu hint session for SurfaceFlinger and HWUI
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_adpf_cpu_hint=true \
+    debug.hwui.use_hint_manager=true
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
