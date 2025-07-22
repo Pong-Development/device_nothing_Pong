0a058aa82b96937241c28b292260a0c93b550e74
chandu078 <chandudyavanapelli03@gmail.com>
2025-07-22 12:55:01 +0530

    Pong: Remove libqtr_sdk.so from blob fixup
    Dropped in ddc3b7f42c4baf7daff9a2a31ec6c6157594fa46, but left in blob fixup.
    Cleaning it up.
    
    Change-Id: Ica68b1b4435d8ff3703d2c296e31c7f795ebeb9b

--- extract-files.py
+++ extract-files.py
@@ -65,7 +65,7 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libofflineproc_jni.so': blob_fixup()
         .add_needed('lib_shim_ntcamera.so'),
-    ('vendor/bin/qcc-trd', 'vendor/lib64/libqtr_sdk.so'): blob_fixup()
+    'vendor/bin/qcc-trd': blob_fixup()
         .replace_needed('libgrpc++_unsecure.so', 'libgrpc++_unsecure_prebuilt.so'),
     ('vendor/bin/hw/android.hardware.gnss-aidl-service-qti', 'vendor/lib64/hw/android.hardware.gnss-aidl-impl-qti.so', 'vendor/lib64/libgarden.so', 'vendor/lib64/libgarden_haltests_e2e.so'): blob_fixup()
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
