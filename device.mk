AOSPA/android_device_nothing_phone2

phone2: Opt out of speaker_layout_channel_mask field
Required for ABI compatibility with audio HAL compiled before 15 QPR2.

Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
Change-Id: Ic910f0634ee0707c21dad77f5264fd89fb33f1c0
Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

device.mk
@@ -49,6 +49,8 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape_qssi/audio_policy_configuration.xml \
     $(DEVICE_PATH)/configs/dirac/interface.json:$(TARGET_COPY_OUT_VENDOR)/etc/dirac/interface.json
 
+$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+
 # AVB
 BOARD_AVB_ENABLE := true
 

Pong-Development/device_nothing_Pong

Signed-off-by: Ghosuto <clash.raja10@gmail.com>

device.mk
@@ -52,6 +52,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     vendor.audio.offload.buffer.size.kb=256
 
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+
 # AVB
 BOARD_AVB_ENABLE := true
 